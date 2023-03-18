10f12640383ebb3c4143bda65c692d5f4bd63b62
Jakub Jelinek <107802809+jakjel@users.noreply.github.com>
2023-03-18 17:52:09 +0100

    Delete java/org/s3/individualassignment/backend directory

--- java/org/s3/individualassignment/backend/business/impl/AccessTokenDecoder.java
+++
@@ -1,7 +0,0 @@
-package org.s3.individualassignment.backend.business.impl;
-
-import org.s3.individualassignment.backend.domain.AccessToken;
-
-public interface AccessTokenDecoder {
-    AccessToken decode(String accessTokenEncoded);
-}
--- java/org/s3/individualassignment/backend/domain/UserRole.java
+++
@@ -1,6 +0,0 @@
-package org.s3.individualassignment.backend.domain;
-
-public enum UserRole {
-    USER,
-    ADMIN
-}
