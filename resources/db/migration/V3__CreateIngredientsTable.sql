bf01798e42107e204fbf1b77aaafd55f5fe78890
Jakub Jelinek <107802809+jakjel@users.noreply.github.com>
2023-03-18 17:52:32 +0100

    Delete resources directory

--- resources/db/migration/V3__CreateIngredientsTable.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE ingredients2
-(
-    ingredient_id         int NOT NULL AUTO_INCREMENT,
-    name_of_ingredient       varchar(50),
-    link        varchar(250),
-    price       double,
-    amount_unit       varchar(20),
-    PRIMARY KEY (ingredient_id)
-);
--- resources/db/migration/V6__CreateGroupsTable.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `expensesgroups`
-(
-    group_id         int NOT NULL AUTO_INCREMENT,
-    created_at       DATE,
-    group_name         varchar(50),
-    PRIMARY KEY (group_id)
-);
-
